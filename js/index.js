5341baffe4f651404df6c4a75d094fa5f014df1f
AdolfHd <sdia7adia@gmail.com>
2023-12-12 22:03:21 +0700

    separated the load of the projects and tsting with one project

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 {
+
+   fetch('./json/projects.json').then((response) => {
+      return response.json().then((data) => {
+         let newHtml = ""
+         for (let key in data) {
+            newHtml += `
+            <div class="proj">
+         <div class="sample" style="background-image: url(${data[key].snapshot});">
+         <h2 class="proj-name">${data[key].name}</h2>
+            <div class="technologies">${data[key].technologies.map((tech) => `<img class="tech" src="./images/${tech}.svg" alt="${tech}">`).join("")}</div>
+            </div>
+            <p class="description">
+            ${data[key].description}
+         </p>
+         <div class="url-link"><a href="${data[key].url}">${data[key].url}</a></div>
+         </div>
+         `
+         }
+         document.getElementById("proj-container").innerHTML = newHtml
+      })
+   }).catch((error) => {
+      console.log("Error", error)
+   })
+
    document.addEventListener('scroll', () => {
       let nav = document.getElementById("nav-bar");
       if (window.scrollY > 0) {
@@ -7,9 +31,10 @@
          nav.classList.remove("blured");
       }
    });
+
    let output = document.getElementById('output-box');
    document.querySelectorAll(".personal-link-img").forEach((elem) => {
       addEventListener('mouseover',
-         (mouse) => mouse.target ? output.innerText = mouse.target.alt: output.innerText = "None");
+         (mouse) => mouse.target ? output.innerText = mouse.target.alt : output.innerText = "None");
    })
 }
