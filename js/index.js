937c97d05316209bd4a885dd690bc2499bc4b109
AdolfHd <sdia7adia@gmail.com>
2024-01-07 23:48:56 +0700

    improved the contact section and added some styles to certs section

--- js/index.js
+++ js/index.js
@@ -35,6 +35,24 @@ let personalLinkHover = (e) => {
       console.log("Error", error)
    })
 
+   fetch('./json/certificates.json').then((response) => {
+      return response.json().then((data) => {
+         let newHtml = ""
+         for (let key in data) {
+            newHtml += `
+            <div class="cert">
+            <img class="cert-img" src="${data[key].src}" alt="${data[key].name}">
+            <p class="cert-text"><a href="${data[key].link}" target="_blank" rel="noopener noreferrer">${data[key].name}</a></p>
+            </div>
+            `
+         }
+         document.getElementById("certs-container").innerHTML = newHtml
+      })
+   })
+   .catch((error) => {
+      console.log("Error", error)
+   })
+
    window.addEventListener('scroll', () => {
       let nav = document.getElementById("nav-bar");
       if (window.scrollY > 5) {
